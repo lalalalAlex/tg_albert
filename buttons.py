ae8fb100ac8219be9de5b0cd175e46da42841571
Your Name <you@example.com>
2023-12-04 20:04:13 +0500

    first

---
+++ buttons.py
@@ -0,0 +1,82 @@
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+def main_buttons():
+
+    buttons = InlineKeyboardMarkup(row_width=2)
+
+    chat_4 = InlineKeyboardButton(text='☁ Чат с GPT-4', callback_data='chat_4')
+    chat_3 = InlineKeyboardButton(text='☁ Чат с GPT-3.5', callback_data='chat_3')
+    generator_photo = InlineKeyboardButton(text='🎨 Генератор фото [DALL-E]', callback_data='photo')
+    voice_helper = InlineKeyboardButton(text='Голосовой помощник', callback_data='voice')
+    text_to_audio = InlineKeyboardButton(text='🗨️ Текст в аудио', callback_data='text_to_voice')
+    audio_to_text = InlineKeyboardButton(text='🔉 Аудио в текст', callback_data='audio_to_text')
+    helper = InlineKeyboardButton(text='🆘 Помощь', callback_data='helper')
+    settings = InlineKeyboardButton(text=' Настройки', callback_data='settings')
+
+    buttons.add(chat_4, chat_3)
+    buttons.row(generator_photo)
+    buttons.row(voice_helper)
+    buttons.row(text_to_audio)
+    buttons.row(audio_to_text)
+    buttons.add(helper, settings)
+
+    return buttons
+
+
+def choice_1():
+    buttons = InlineKeyboardMarkup(row_width=2)
+    bez = InlineKeyboardButton(text='Без контекста', callback_data='not')
+    c = InlineKeyboardButton(text='С контекстом', callback_data='with')
+    back = InlineKeyboardButton(text='Назад', callback_data='back')
+
+    buttons.add(bez, c)
+    buttons.row(back)
+
+    return buttons
+
+
+def choice_2():
+    buttons = InlineKeyboardMarkup(row_width=1)
+    try_ = InlineKeyboardButton(text='Попробовать', callback_data='try')
+    back = InlineKeyboardButton(text='Назад', callback_data='back')
+
+    buttons.add(try_, back)
+
+    return buttons
+
+
+def choice_3():
+    buttons = InlineKeyboardMarkup(row_width=1)
+    start_dialogue = InlineKeyboardButton(text='Начать диалог', callback_data='start_dialogue')
+    back = InlineKeyboardButton(text='Назад', callback_data='back')
+
+    buttons.add(start_dialogue, back)
+
+    return buttons
+
+
+def choice_4():
+    buttons = InlineKeyboardMarkup(row_width=2)
+    alloy = InlineKeyboardButton(text='alloy', callback_data='alloy')
+    echo = InlineKeyboardButton(text='echo', callback_data='echo')
+    nova = InlineKeyboardButton(text='nova', callback_data='nova')
+    fable = InlineKeyboardButton(text='fable', callback_data='fable')
+    shimmer = InlineKeyboardButton(text='shimmer', callback_data='shimmer')
+    back = InlineKeyboardButton(text='Назад', callback_data='back')
+
+    buttons.add(alloy, echo, fable, nova)
+    buttons.row(shimmer)
+    buttons.row(back)
+
+    return buttons
+
+
+def choice_5():
+    buttons = InlineKeyboardMarkup(row_width=1)
+    try_ = InlineKeyboardButton(text='Попробовать', callback_data='try')
+    back = InlineKeyboardButton(text='Назад', callback_data='back')
+
+    buttons.add(try_, back)
+
+    return buttons
---
+++ main.py
@@ -0,0 +1,93 @@
+import telebot
+import buttons
+
+bot = telebot.TeleBot('6720495897:AAG6XPI9muh2d52afJLTkByYQH4ektLq6hw')
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    global user_id
+    user_id = message.from_user.id
+    bot.send_message(user_id, 'Привет' + message.from_user.first_name + message.from_user.last_name, reply_markup=buttons.main_buttons())
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_checker(call):
+    if call.data == 'chat_4':
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                              text='🤖 GPT-4:\n'
+
+                                    '📋 Используется самая последняя языковая модель GPT-4 Turbo. Умеет описывать ваши фотографии.\n'
+                                    
+                                    '🔘 Принимает текст, голос или фото.\n'
+                                    
+                                    '🗯 Чат без контекста — не учитывает контекст, каждый ваш запрос как новый диалог.\n'
+                                    '⚡️ 1 запрос = 3 GT\n'
+                                    
+                                    '💬 Чат с контекстом — каждый ответ с учетом контекста вашего диалога.\n'
+                                    '⚡️ 1 запрос = 4 GT\n'
+                                    
+                                    '└ Выберите чат:', reply_markup=buttons.choice_1())
+
+    elif call.data == 'chat_3':
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                              text='🤖 GPT-3.5:\n'
+
+                                    '📋 Используется самая экономически эффективная модель GPT-3.5 Turbo.\n'
+
+                                    '🔘 Принимает текстовое или голосовое сообщение.\n'
+
+                                    '🗯 Чат без контекста — не учитывает контекст, каждый ваш запрос как новый диалог.\n'
+                                    '⚡️ 1 запрос = 1 GT\n'
+                                    
+                                    '💬 Чат с контекстом — каждый ответ с учетом контекста вашего диалога.\n'
+                                    '⚡️ 1 запрос = 2 GT', reply_markup=buttons.choice_1())
+
+    elif call.data == 'photo':
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                              text='🤖 DALL·E:\n' 
+                                   
+                                    '📋 DALL·E 3 — это нейросеть, которая может создавать реалистичные изображения и произведения искусства на основе вашего описания.\n'
+                                    
+                                    '🔘 Принимает описание: текстовое или голосовое сообщение, либо фото.\n'
+                                    
+                                    '⚡️ 1 запрос = 12 GT\n'
+                                    
+                                    '└ Сгенерировать изображение:', reply_markup=buttons.choice_2())
+    elif call.data == 'voice':
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                              text='🤖 Голосовой ассистент:\n'
+
+                                    '📋 Первый голосовой ассистент в Telegram на базе ChatGPT.\n'
+                                    
+                                    '🔘 Принимает голосовое сообщение.\n'
+                                    
+                                    '⚡️ 1 запрос = 3 GT', reply_markup=buttons.choice_3())
+
+    elif call.data == 'text_to_voice':
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                              text='🤖 Text-To-Speech:\n'
+                                        
+                                        '📋 Модель искусственного интеллекта, которая преобразует текст в естественно звучащий устный текст.\n'
+                                        
+                                        '⚡️ 1 запрос = 3 GT\n'
+                                        
+                                        '└ Выберите голос:', reply_markup=buttons.choice_4())
+
+    elif call.data == 'audio_to_text':
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                              text='🤖 Speech-To-Text:\n'
+                                    
+                                    '📋 Модель ИИ, которая преобразует ваше аудио в текст.\n'
+                                    
+                                    '⚡️ 1 запрос = 2 GT\n'
+                                    
+                                    '└ Попробуйте:', reply_markup=buttons.choice_5())
+
+    elif call.data == 'back':
+        bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                              text=f'Привет {call.from_user.first_name} {call.from_user.last_name}',
+                              reply_markup=buttons.main_buttons())
+
+
+bot.infinity_polling()
